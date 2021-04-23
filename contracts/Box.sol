2ac59a6e78ce32411b342d60f964702381784595
Max Tyrrell <maxtyrrell0@gmail.com>
2021-04-24 00:13:49 +0200

    refactor: replace our Auth with Ownable
    Use OpenZeppelin's Ownable contract rather than our Auth contract to
    access-control Box.

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,24 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.3;
 
+// Import Ownable from the OpenZeppelin Contracts Library
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "./access-control/Auth.sol";
 
-contract Box {
+contract Box is Ownable {
   uint256 private value;
-  Auth private auth;
 
   // Emitted when the stored value changes
   event ValueChanged(uint256 newValue);
 
-  constructor(Auth _auth) public {
-    auth = _auth;
-  }
-
   // Stores a new value in the contract
-  function store(uint256 newValue) public {
-    // Require that the caller is registered as an administrator in Auth
-    require(auth.isAdministrator(msg.sender), "Unauthorised");
-
+  function store(uint256 newValue) public onlyOwner {
     value = newValue;
     emit ValueChanged(newValue);
   }
