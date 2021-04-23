8f954452781b18b13962c0d517b87c1c76a5c23b
Max Tyrrell <maxtyrrell0@gmail.com>
2021-04-24 00:08:48 +0200

    feat: require administrator access to mutate value

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,14 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.3;
 
+import "./access-control/Auth.sol";
+
 contract Box {
   uint256 private value;
+  Auth private auth;
 
   // Emitted when the stored value changes
   event ValueChanged(uint256 newValue);
 
+  constructor(Auth _auth) public {
+    auth = _auth;
+  }
+
   // Stores a new value in the contract
   function store(uint256 newValue) public {
+    // Require that the caller is registered as an administrator in Auth
+    require(auth.isAdministrator(msg.sender), "Unauthorised");
+
     value = newValue;
     emit ValueChanged(newValue);
   }
