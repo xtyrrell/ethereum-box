eca4d2cbc5914c5bc5b294ead55b761e10f0774b
Max Tyrrell <maxtyrrell0@gmail.com>
2021-04-25 23:02:32 +0200

    feat: print deploying account info

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,13 @@
 async function main() {
+  const [ deployer ] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
   const Box = await ethers.getContractFactory("Box")
   console.log("Deploying Box...")
 
